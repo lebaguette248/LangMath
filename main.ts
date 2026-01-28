ac345a59ffb7d33f0b3a2623f61bdbc647c0a05d
Nicky Lopez <nickylopez@outlook.de>
2026-01-28 08:28:37 +0100

    Removed: Templating stuff thats not needed

--- main.ts
+++ main.ts
@@ -19,9 +19,10 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('dice', 'LangMathOllama', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+			new Notice('Converting to LaTeX..');
+
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
@@ -102,36 +103,6 @@ export default class MyPlugin extends Plugin {
 			}
 		});
 
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-
-
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new LangMathSettingTab(this.app, this));
@@ -196,8 +167,17 @@ class LangMathSettingTab extends PluginSettingTab {
 				.setValue(this.plugin.settings.myApiKey ?? '')
 				.onChange(async (value) => {
 					this.plugin.settings.myApiKey = value;
-					console.log(`the new Registered API Key is: ${this.plugin.settings.myApiKey}`);
-					console.log(`with ${typeof this.plugin.settings.myApiKey}`);
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Model')
+			.setDesc('Enter the exact type of model, for example llama3.1:8b')
+			.addText(text => text
+				.setPlaceholder('llama3.1:8b')
+				.setValue(this.plugin.settings.myCustomModel?? '')
+				.onChange(async (value) => {
+					this.plugin.settings.myCustomModel = value;
 					await this.plugin.saveSettings();
 				}));
 	}
